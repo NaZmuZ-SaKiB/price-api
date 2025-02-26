6953bc2d1001ebdc8524e61470c54061dfb989b9
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-02-26 17:55:31 +0600

    Add PriceUpdates component to integrate UpdateProducts

---
+++ src/app/price-updates/page.tsx
@@ -0,0 +1,11 @@
+import UpdateProducts from "./_components/UpdateProducts";
+
+const PriceUpdates = () => {
+  return (
+    <div>
+      <UpdateProducts />
+    </div>
+  );
+};
+
+export default PriceUpdates;
