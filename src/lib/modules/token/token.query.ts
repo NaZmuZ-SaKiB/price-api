b273950d5fe7ba62ccdcde07442bd4eb6795829d
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-02-26 19:06:19 +0600

    Add token query and mutation hooks for token management

---
+++ src/lib/modules/token/token.query.ts
@@ -0,0 +1,36 @@
+import { Tags } from "@/constants/tags";
+import {
+  tokenCreateAction,
+  tokenDeleteByIdAction,
+  tokenDeleteExpiredAction,
+  tokenGetAllAction,
+  tokenGetByIdAction,
+} from "./token.action";
+import { useMutation, useQuery } from "@tanstack/react-query";
+
+export const useTokenGetAllQuery = (params: string) =>
+  useQuery({
+    queryKey: [Tags.TOKEN, Tags.ALL, params],
+    queryFn: () => tokenGetAllAction(params),
+  });
+
+export const useTokenGetByIdQuery = (id: string) =>
+  useQuery({
+    queryKey: [Tags.TOKEN, id],
+    queryFn: () => tokenGetByIdAction(id),
+  });
+
+export const useTokenCreateMutation = () =>
+  useMutation({
+    mutationFn: tokenCreateAction,
+  });
+
+export const useTokenDeleteByIdMutation = () =>
+  useMutation({
+    mutationFn: tokenDeleteByIdAction,
+  });
+
+export const useTokenDeleteExpiredMutation = () =>
+  useMutation({
+    mutationFn: tokenDeleteExpiredAction,
+  });
