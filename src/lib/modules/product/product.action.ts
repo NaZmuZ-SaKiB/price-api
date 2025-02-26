84a97656b8f52c04566f1453ec910cdff1c5196d
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-02-26 13:48:00 +0600

    Add productUpdateAction for updating product details via PATCH request

--- src/lib/modules/product/product.action.ts
+++ src/lib/modules/product/product.action.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { BACKEND_URL } from "@/constants";
+import { TProduct } from "./product.type";
 
 export const productGetAllAction = async (params: string) => {
   const response = await fetch(`${BACKEND_URL}/api/product?${params}`, {
@@ -19,6 +20,27 @@ export const productGetAllAction = async (params: string) => {
       meta: {},
     };
   }
+
+  return result;
+};
+
+export const productUpdateAction = async ({
+  id,
+  payload,
+}: {
+  id: string;
+  payload: Partial<TProduct>;
+}) => {
+  const response = await fetch(`${BACKEND_URL}/api/product/${id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+    cache: "no-store",
+  });
+
+  const result = await response.json();
 
   return result;
 };
