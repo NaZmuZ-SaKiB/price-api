221057c28203aa6b00966bafabe234a8d547bc11
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-02-26 13:48:08 +0600

    Add useProductUpdateQuery for product update mutations

--- src/lib/modules/product/product.query.ts
+++ src/lib/modules/product/product.query.ts
@@ -1,9 +1,14 @@
 import { Tags } from "@/constants/tags";
-import { useQuery } from "@tanstack/react-query";
-import { productGetAllAction } from "./product.action";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { productGetAllAction, productUpdateAction } from "./product.action";
 
 export const useProductGetAllQuery = (params: string) =>
   useQuery({
     queryKey: [Tags.PRODUCT, Tags.ALL, params],
     queryFn: () => productGetAllAction(params),
+  });
+
+export const useProductUpdateQuery = () =>
+  useMutation({
+    mutationFn: productUpdateAction,
   });
