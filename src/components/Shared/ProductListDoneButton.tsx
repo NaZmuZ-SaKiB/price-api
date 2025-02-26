b3e5385fcf548f77f8c5c7440c843494224d0cfa
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-02-26 15:32:39 +0600

    Add ProductListDoneButton component for marking products as done

---
+++ src/components/Shared/ProductListDoneButton.tsx
@@ -0,0 +1,45 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use client";
+
+import { useProductUpdateQuery } from "@/lib/modules/product/product.query";
+import { Button } from "../ui/button";
+import { useState } from "react";
+import { toast } from "sonner";
+
+type TProps = {
+  id: string;
+  defaultDone: boolean;
+};
+
+const ProductListDoneButton = ({ id, defaultDone }: TProps) => {
+  const [done, setDone] = useState(defaultDone);
+  const { mutateAsync: updateProduct, isPending } = useProductUpdateQuery();
+
+  const handleClick = async () => {
+    try {
+      const result = await updateProduct({ id, payload: { done: true } });
+
+      if (result?.success) {
+        setDone(true);
+        toast.success(result?.message);
+      } else {
+        toast.error(result?.message || "A Client Error Occured.");
+      }
+    } catch (error: any) {
+      toast.error(error?.message || "A Server Error Occured.");
+    }
+  };
+
+  return (
+    <Button
+      disabled={isPending || done}
+      className="disabled:bg-slate-300 disabled:text-slate-500"
+      size="sm"
+      onClick={handleClick}
+    >
+      {isPending ? "Updating..." : done ? "Done" : "Mark As Done"}
+    </Button>
+  );
+};
+
+export default ProductListDoneButton;
