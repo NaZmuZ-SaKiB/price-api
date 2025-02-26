8343a8bb74e0b6435e734ed96e3ac3c34ee9e88b
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-02-26 17:55:25 +0600

    Add UpdateProducts component for managing product updates with pagination and sorting

---
+++ src/app/price-updates/_components/UpdateProducts.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import DataPagination from "@/components/Shared/DataPagination";
+import DataSearchBox from "@/components/Shared/DataSearchBox";
+import DataSortBySelect from "@/components/Shared/DataSortBySelect";
+import DataSortOrderSelect from "@/components/Shared/DataSortOrderSelect";
+import ProductList from "@/components/Shared/ProductList";
+import { useProductGetAllQuery } from "@/lib/modules/product/product.query";
+import { TProduct } from "@/lib/modules/product/product.type";
+import { useSearchParams } from "next/navigation";
+
+const UpdateProducts = () => {
+  const searchParams = useSearchParams();
+
+  const { data: productData, isLoading: productLoading } =
+    useProductGetAllQuery(`${searchParams.toString()}&done=false`);
+  const products: TProduct[] = productData?.data || [];
+
+  if (productLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <DataSearchBox className="mb-4" />
+      <div className="mb-4 flex gap-4">
+        <DataSortBySelect options={["lastModified", "createdAt", "price"]} />
+        <DataSortOrderSelect />
+      </div>
+      <ProductList products={products} highlight />
+      <DataPagination
+        className="mt-4"
+        limit={productData?.meta?.limit}
+        page={productData?.meta?.page}
+        total={productData?.meta?.total}
+      />
+    </div>
+  );
+};
+
+export default UpdateProducts;
