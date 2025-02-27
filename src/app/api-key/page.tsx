d835a32ecfb69299d4bb4c874137bb861cd5b585
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-02-27 15:16:24 +0600

    Enhance ApiKeyPage layout with title and improved alignment

--- src/app/api-key/page.tsx
+++ src/app/api-key/page.tsx
@@ -6,7 +6,8 @@ import { Plus } from "lucide-react";
 const ApiKeyPage = () => {
   return (
     <div>
-      <div className="flex justify-end mb-4">
+      <div className="flex justify-between items-center mb-4">
+        <h1 className="text-xl uppercase font-bold text-slate-700">Api Keys</h1>
         <Link href="/api-key/add">
           <Button className="rounded-none">
             <Plus /> Add New
