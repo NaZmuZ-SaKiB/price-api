7a104dce60a3e8508df6f6a592c794e9bb40610d
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-02-28 22:33:27 +0600

    feat: add SignInPage component to render SignInForm

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import SignInForm from "./SignInForm";
+
+const SignInPage = () => {
+  return (
+    <div className="h-[80svh] grid place-items-center">
+      <SignInForm />
+    </div>
+  );
+};
+
+export default SignInPage;
