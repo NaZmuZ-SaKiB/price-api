9e8bbfba4bdeb1c4e7498bf96a6bfa186e4df53d
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-02-28 22:38:05 +0600

    feat: add isTokenValidAction to verify JWT token validity

--- src/lib/modules/auth/auth.action.ts
+++ src/lib/modules/auth/auth.action.ts
@@ -1,7 +1,10 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use server";
 
 import { AUTH_KEY, BACKEND_URL } from "@/constants";
+import { jwtHelpers } from "@/utils/jwtHelpers";
 import { cookies } from "next/headers";
+import { TTokenAccess } from "../token/token.type";
 
 export const signInAction = async (payload: { token: string }) => {
   const response = await fetch(`${BACKEND_URL}/api/token/sign-in`, {
@@ -32,4 +35,26 @@ export const signInAction = async (payload: { token: string }) => {
 
 export const signOutAction = async () => {
   (await cookies()).delete(AUTH_KEY);
+};
+
+export const isTokenValidAction = async (): Promise<{
+  token: string;
+  access: TTokenAccess;
+} | null> => {
+  try {
+    const jwt = (await cookies()).get(AUTH_KEY);
+    if (!jwt?.value) {
+      return null;
+    }
+
+    const decoded = (await jwtHelpers.verifyToken(
+      jwt.value,
+      process.env.JWT_SECRET as string
+    )) as unknown as { payload: any };
+
+    return decoded.payload;
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (error) {
+    return null;
+  }
 };
