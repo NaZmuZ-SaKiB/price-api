4705586d27f3e2a0e561c33762e2dca43a940728
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-02-28 21:00:27 +0600

    refactor: moved all to root layout

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "../globals.css";
 import { SidebarTrigger } from "@/components/ui/sidebar";
 import AppSidebar from "@/components/Layout/AppSidebar";
 import Providers from "@/lib/providers/Providers";
