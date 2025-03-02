1746190f46080c565df2588fa0a0110b8ee99965
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-03-02 13:33:50 +0600

    feat: add authorization header to scrape action for secure API requests

--- src/lib/modules/scrape/scrape.action.ts
+++ src/lib/modules/scrape/scrape.action.ts
@@ -1,12 +1,14 @@
 "use server";
 
-import { BACKEND_URL } from "@/constants";
+import { AUTH_KEY, BACKEND_URL } from "@/constants";
+import { cookies } from "next/headers";
 
 export const scrapeAction = async (url: string) => {
   const response = await fetch(`${BACKEND_URL}/api/scrape`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
+      Authorization: (await cookies()).get(AUTH_KEY)?.value || "",
     },
     body: JSON.stringify({ url }),
     cache: "no-store",
