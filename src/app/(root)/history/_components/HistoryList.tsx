d1f8b4793a645df304867e7178e1fe5de873fef8
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-03-02 13:34:04 +0600

    feat: implement HistoryList component to display scraped history data with pagination

---
+++ src/app/(root)/history/_components/HistoryList.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import DataPagination from "@/components/Shared/DataPagination";
+import { useHistoryGetAllQuery } from "@/lib/modules/history/history.query";
+import { THistory } from "@/lib/modules/history/history.type";
+import formatTime12HrFormat from "@/utils/formatTime12Hr";
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
+
+const HistoryList = () => {
+  const searchParams = useSearchParams();
+  const { data: historyData, isLoading } = useHistoryGetAllQuery(
+    searchParams.toString()
+  );
+  const history: THistory[] = historyData?.data || [];
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <div>
+      <table className="table table-auto primary-table">
+        <thead>
+          <tr>
+            <th>URL</th>
+            <th>Date</th>
+            <th>Time</th>
+            <th>Scraped By</th>
+          </tr>
+        </thead>
+        <tbody>
+          {history.map((history) => (
+            <tr key={history._id.toString()}>
+              <td>
+                <Link
+                  className="text-sky-600 hover:underline"
+                  href={history.url}
+                >
+                  {history.url}
+                </Link>
+              </td>
+              <td className="text-center">
+                {new Date(history.createdAt).toLocaleDateString()}
+              </td>
+              <td className="text-center">
+                {formatTime12HrFormat(new Date(history.createdAt))}
+              </td>
+              <td className="text-center">{history.scrapedBy.name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <DataPagination
+        className="mt-4"
+        limit={historyData?.meta?.limit}
+        page={historyData?.meta?.page}
+        total={historyData?.meta?.total}
+      />
+    </div>
+  );
+};
+
+export default HistoryList;
