51362cf0995b38c83bc62fd5edd2c20ebf071f3a
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-03-04 16:25:19 +0600

    feat: conditionally render ClearHistoryButton based on admin access

--- src/app/(root)/history/page.tsx
+++ src/app/(root)/history/page.tsx
@@ -3,13 +3,16 @@ import DataSortBySelect from "@/components/Shared/DataSortBySelect";
 import DataSortOrderSelect from "@/components/Shared/DataSortOrderSelect";
 import HistoryList from "./_components/HistoryList";
 import ClearHistoryButton from "./_components/ClearHistoryButton";
+import { isTokenValidAction } from "@/lib/modules/auth/auth.action";
 
-const HistoryPage = () => {
+const HistoryPage = async () => {
+  const data = await isTokenValidAction();
+
   return (
     <div>
       <div className="flex justify-between items-center mb-4">
         <h1 className="text-xl uppercase font-bold text-slate-700">History</h1>
-        <ClearHistoryButton />
+        {data?.access === "admin" && <ClearHistoryButton />}
       </div>
 
       <DataSearchBox className="mb-4" />
