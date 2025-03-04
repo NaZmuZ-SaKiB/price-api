498b163ae4f0599013c490b3c7eab751f673f7b7
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-03-04 19:15:50 +0600

    feat: enhance ApiKeyList to visually indicate expired API keys

--- src/app/(root)/api-key/_components/ApiKeyList.tsx
+++ src/app/(root)/api-key/_components/ApiKeyList.tsx
@@ -37,56 +37,63 @@ const ApiKeyList = () => {
           </tr>
         </thead>
         <tbody>
-          {apiKeys.map((apiKey) => (
-            <tr key={apiKey._id}>
-              <td className="w-[200px]">
-                <div className="flex items-center justify-between">
-                  <span>{viewToken ? apiKey.token : "********"}</span>
+          {apiKeys.map((apiKey) => {
+            const isExpired =
+              new Date(apiKey.exp) < new Date() && apiKey.access !== "admin";
+            return (
+              <tr
+                key={apiKey._id}
+                className={isExpired ? "opacity-50 bg-slate-50" : ""}
+              >
+                <td className="w-[200px]">
+                  <div className="flex items-center justify-between">
+                    <span>{viewToken ? apiKey.token : "********"}</span>
 
-                  <Button
-                    variant="outline"
-                    size="icon"
-                    className="cursor-pointer"
-                    onClick={() => setViewToken((prev) => !prev)}
-                  >
-                    {viewToken ? (
-                      <EyeOff className="w-4" />
-                    ) : (
-                      <Eye className="w-4" />
-                    )}
-                  </Button>
-                </div>
-              </td>
-              <td>{apiKey.name}</td>
-              <td>{apiKey.access}</td>
-              <td>{new Date(apiKey.exp).toLocaleDateString()}</td>
-              <td>
-                <div className="flex items-center justify-center gap-2">
-                  <Button
-                    variant="outline"
-                    size="icon"
-                    className="cursor-pointer rounded-none"
-                    onClick={() => {
-                      navigator.clipboard.writeText(apiKey.token);
-                      toast.message("Copied to clipboard.");
-                    }}
-                  >
-                    <Copy className="w-4" />
-                  </Button>
-
-                  <ApiKeyDeleteButton id={apiKey._id?.toString()}>
                     <Button
-                      className="rounded-none text-red-500 hover:text-red-500"
                       variant="outline"
                       size="icon"
+                      className="cursor-pointer"
+                      onClick={() => setViewToken((prev) => !prev)}
                     >
-                      <Trash2 />
+                      {viewToken ? (
+                        <EyeOff className="w-4" />
+                      ) : (
+                        <Eye className="w-4" />
+                      )}
                     </Button>
-                  </ApiKeyDeleteButton>
-                </div>
-              </td>
-            </tr>
-          ))}
+                  </div>
+                </td>
+                <td>{apiKey.name}</td>
+                <td>{apiKey.access}</td>
+                <td>{new Date(apiKey.exp).toLocaleDateString()}</td>
+                <td>
+                  <div className="flex items-center justify-center gap-2">
+                    <Button
+                      variant="outline"
+                      size="icon"
+                      className="cursor-pointer rounded-none"
+                      onClick={() => {
+                        navigator.clipboard.writeText(apiKey.token);
+                        toast.message("Copied to clipboard.");
+                      }}
+                    >
+                      <Copy className="w-4" />
+                    </Button>
+
+                    <ApiKeyDeleteButton id={apiKey._id?.toString()}>
+                      <Button
+                        className="rounded-none text-red-500 hover:text-red-500"
+                        variant="outline"
+                        size="icon"
+                      >
+                        <Trash2 />
+                      </Button>
+                    </ApiKeyDeleteButton>
+                  </div>
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
 
