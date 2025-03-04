0a423ea08becb6a1043c338b00c6ccc02055bd6a
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-03-04 15:52:26 +0600

    feat: enhance middleware to verify admin access for api-key routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,8 @@ import { AUTH_KEY } from "./constants";
 import { jwtHelpers } from "./utils/jwtHelpers";
 
 export async function middleware(request: NextRequest) {
+  const pathName = request.nextUrl.pathname;
+
   const jwt = request.cookies.get(AUTH_KEY);
 
   if (!jwt?.value) {
@@ -11,7 +13,14 @@ export async function middleware(request: NextRequest) {
   }
 
   try {
-    await jwtHelpers.verifyToken(jwt.value, process.env.JWT_SECRET as string);
+    const decoded = await jwtHelpers.verifyToken(
+      jwt.value,
+      process.env.JWT_SECRET as string
+    );
+
+    if (pathName.includes("api-key") && decoded.payload?.access !== "admin") {
+      return NextResponse.redirect(new URL(`/sign-in`, request.url));
+    }
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
   } catch (error: any) {
@@ -21,15 +30,12 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - /sign-in (case insensitive)
-     * - /api (API routes)
-     * - /_next/static (static files)
-     * - /_next/image (Next.js image optimization)
-     * - /favicon.ico (favicon file)
-     * - /public (public files)
-     */
-    "/((?!sign-in|api|_next/static|_next/image|favicon.ico|public).*)",
+    "/",
+    "/products",
+    "/price-updates",
+    "/scrape",
+    "/saved-urls",
+    "/history",
+    "/api-key",
   ],
 };
